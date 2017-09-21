a7b7ebbb201c42eb2ba5ee9cf7e9947e4a3a07fa
WebmetsHD <webmets.alt1@gmail.com>
2017-09-21 20:57:51 +0100

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,20 @@
+package me.oreo.bots.mainbot;
+
+public class Constants {
+	
+	static String COPYRIGHT = "\u00a9";
+	
+	public static final String COMMAND_PREFIX = "!";
+	
+    public static final String BOT_VERSION = "1";
+
+    
+    public static final String EMBED_AUTHOR = "Eliza";
+    public static final String EMBED_AUTHOR_IMAGE = "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg";
+    public static final String EMBED_AUTHOR_URL = "https://github.com/webmetsHD";
+    
+    
+    public static final String EMBED_FOOTER_NAME = "RareMeme" + COPYRIGHT;
+    public static final String EMBED_FOOTER_IMAGE = null;
+    
+}

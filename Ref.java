5bcfd97054a274ae39d41a40e9eb217622f19c63
WebmetsHD <webmets.alt1@gmail.com>
2017-09-21 20:57:00 +0100

    Create Ref.java

---
+++ Ref.java
@@ -0,0 +1,13 @@
+package me.oreo.bots.mainbot;
+
+public class Ref {
+	
+	public static final String TOKEN = "MzU5NDI5MTA5MzE2NzE0NTA2.DKWhEg.ZyvmvfJXyU6Rtw2oVO9LMmMUoGM";
+	public static final String CLINET_ID = "359429109316714506";
+	public static final String PREFIX = "!-";
+	
+	public static final boolean isBOT = true;
+	public static final String BOT_INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=CLIENT_ID&scope=bot&permissions=268454912";
+	public static final String API_BASE_URL = "https://launchlibrary.net/1.2";
+
+}
